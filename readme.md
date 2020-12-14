c269bfe2d637b91868287bb34cd4da57088e26df
stepanyanhayk <haykstepanyan02@gmail.com>
2020-12-14 15:51:49 +0400

    improved accuracy and modified readme

--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 **RECOGNISING TRAFFIC SIGNS**<br>
-43 different categories <br/>
+<b>Dataset: <a href="https://www.kaggle.com/meowmeowmeowmeowmeow/gtsrb-german-traffic-sign">German Traffic Sign Recognition Benchmark (GTSRB)</a></b><br><br>
+
+The German Traffic Sign Benchmark is a multi-class, single-image classification challenge held at the International Joint Conference on Neural Networks (IJCNN) 2011. 
+
+<li>43 classes</li>
+<li>More than 50,000 images in total</li>
+
+
+<br/>
+Accuracy achieved: 97.5%
 <br/>
 
-10 epochs.
-<br/>
-Accuracy: 97.3%
+Usage: python traffic.py data_directory [model.h5]<br>
+<li>data_directory is the directory where the photos are stored</li>
+<li>model.h5 (optional) is the filename in which user wants to store the final model</li>
+e.g. python3 traffic.py gtsrb model.h5
--- traffic.py
+++ traffic.py
@@ -1,5 +1,5 @@
 """
-Project Name: Traffic
+TRAFFIC IMAGE CLASSIFICATION
 
 @author: Hayk Stepanyan
 """
@@ -9,108 +9,64 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
-
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 10
-IMG_WIDTH = 30
-IMG_HEIGHT = 30
+EPOCHS = 15
+IMG_WIDTH, IMG_HEIGHT = 30, 30
 NUM_CATEGORIES = 43
-TEST_SIZE = 0.4
-
-
-def main():
-
-    # Check command-line arguments
-    if len(sys.argv) not in [2, 3]:
-        sys.exit("Usage: python traffic.py data_directory [model.h5]")
-
-    # Get image arrays and labels for all image files
-    images, labels = load_data("gtsrb")
-
-    # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
-    x_train, x_test, y_train, y_test = train_test_split(
-        np.array(images), np.array(labels), test_size=TEST_SIZE
-    )
-
-    # Get a compiled neural network
-    model = get_model()
-
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
-
-    # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
-
-    # Save model to file
-    if len(sys.argv) == 3:
-        filename = sys.argv[2]
-        model.save(filename)
-        print(f"Model saved to {filename}.")
-
+TEST_SIZE = 0.3
 
 def load_data(data_dir):
     """
-    Load image data from directory `data_dir`.
-
-    Assume `data_dir` has one directory named after each category, numbered
-    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
-    number of image files.
-
-    Return tuple `(images, labels)`. `images` should be a list of all
-    of the images in the data directory, where each image is formatted as a
-    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
-    be a list of integer labels, representing the categories for each of the
-    corresponding `images`.
+    Args:
+        data_dir - directory
+    Return:
+        tuple (images, labels), where images is the list of images and 
+        labels is a list of integers representing corresponding labels
     """
     images, labels = [], []
     for category in range(43):
-        images_list = os.listdir(os.path.join(data_dir, str(category)))
-        for image_name in images_list:
-            image = cv2.imread(os.path.join(data_dir, str(category), str(image_name)))
+        for image_name in os.listdir(os.path.join(data_dir, str(category))):
+            image = cv2.imread(os.path.join(data_dir, str(category), image_name))
             images.append(cv2.resize(image, (30, 30)))
             labels.append(category)
     return images, labels
 
-
 def get_model():
     """
-    Returns a compiled convolutional neural network model. Assume that the
-    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
-    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    Return:
+        Trained CNN
     """
-    # Create a convolutional neural network
     model = tf.keras.models.Sequential([
-
-        # Convolutional layer. Learn 32 filters using a 4x4 kernel
         tf.keras.layers.Conv2D(
             32, (4, 4), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
-        # Convolutional layer. Learn 64 filters using a 4x4 kernel
         tf.keras.layers.Conv2D(
             64, (4, 4), activation="relu"
         ),
 
-        # Max Pooling
         tf.keras.layers.MaxPooling2D(2, 2),
 
-        # Flatten units
+        tf.keras.layers.Conv2D(
+            10, (3, 3)
+        ),
+
+        tf.keras.layers.MaxPooling2D(2, 2),
+
+        tf.keras.layers.Conv2D(
+            10, (3, 3)
+        ),
+
         tf.keras.layers.Flatten(),
-
-        # Add a hidden layer with dropout
         tf.keras.layers.Dense(256, activation="relu"),
-
-        # Add a hidden layer with dropout
         tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dropout(0.3),
-
-        # Add an output layer with output units for all 43 categories
+        tf.keras.layers.Dense(100, activation="relu"),
+        tf.keras.layers.Dropout(0.3),
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
 
-    # Train neural network
     model.compile(
         optimizer="adam",
         loss="categorical_crossentropy",
@@ -120,5 +76,24 @@ def get_model():
     return model
 
 
-if __name__ == "__main__":
-    main()
+if len(sys.argv) not in [2, 3]:
+    sys.exit("Usage: python traffic.py data_directory [model.h5]")
+
+images, labels = load_data("gtsrb")
+
+# Split dataset into training and test
+labels = tf.keras.utils.to_categorical(labels)
+x_train, x_test, y_train, y_test = train_test_split(
+    np.array(images), np.array(labels), test_size=TEST_SIZE
+)
+
+
+model = get_model()
+model.fit(x_train, y_train, epochs=EPOCHS)
+model.evaluate(x_test,  y_test, verbose=2)
+
+# Save model to file
+if len(sys.argv) == 3:
+    filename = sys.argv[2]
+    model.save(filename)
+    print(f"Model saved to {filename}.")
